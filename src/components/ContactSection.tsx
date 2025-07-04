d0c4cafbebf773ae9b9b58eab0e972c40ae980a1
Oussama El fila <121666434+elfilaoussama@users.noreply.github.com>
2025-07-04 12:39:09 +0100

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -54,7 +54,9 @@ export default function ContactSection({ contact }: ContactSectionProps) {
                 </div>
                 <div className="flex items-center">
                   <Linkedin className="text-primary w-5 h-5 mr-3" />
-                  <span>{contact.linkedin}</span>
+                  <a href={`https://${contact.linkedin}`} className="text-[#2094F3] hover:underline">
+                    {contact.linkedin}
+                  </a>
                 </div>
               </div>
             </div>
